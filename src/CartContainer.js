356cab68e6cd4a0a49ea6de7d1ac1b40f07b5535
kiu_ushkhvani.ana <ushkhvani.ana@kiu.edu.ge>
2023-05-09 13:32:25 +0400

    first commit

---
+++ src/CartContainer.js
@@ -0,0 +1,43 @@
+import React from "react";
+import CartItem from "./CartItem";
+import { useGlobalContext } from "./context";
+
+const CartContainer = () => {
+  const { cart, total, clearCart } = useGlobalContext();
+  if (cart.length === 0) {
+    return (
+      <section className="cart">
+        <header>
+          <h2>YOUR BAG</h2>
+          <h4 className="empty-cart">is currently empty</h4>
+        </header>
+      </section>
+    );
+  }
+  return (
+    <section className="cart">
+      <header>
+        <h2>YOUR BAG</h2>
+      </header>
+
+      <div>
+        {cart.map((item) => {
+          return <CartItem key={item.id} {...item} />;
+        })}
+      </div>
+      <footer>
+        <hr />
+        <div className="cart-total">
+          <h4>
+            Total <span>${total}</span>
+          </h4>
+        </div>
+        <button className="btn clear" onClick={clearCart}>
+          CLEAR CART
+        </button>
+      </footer>
+    </section>
+  );
+};
+
+export default CartContainer;
---
+++ src/data.js
@@ -0,0 +1,26 @@
+import iphone7 from "./img/Iphone-7-plus.png";
+import iphoneXr from "./img/Iphone-xr-yellow.png";
+import iphone11 from "./img/IPhone-11-pro-Max.png";
+export const DATA = [
+  {
+    id: 1,
+    title: "IPhone 11 pro max",
+    price: 999.62,
+    img: iphone11,
+    amount: 1,
+  },
+  {
+    id: 2,
+    title: "IPhone XR",
+    price: 499.99,
+    img: iphoneXr,
+    amount: 1,
+  },
+  {
+    id: 3,
+    title: "IPhone 7 plus",
+    price: 769.99,
+    img: iphone7,
+    amount: 1,
+  },
+];
